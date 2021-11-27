8a46d6918eb6e7a84f917765f490b9fb1502aa35
Leinadium <daniel.sch.guima@gmail.com>
2021-11-27 05:35:40 -0300

    criando view de dashboard

--- flask/app/home/views.py
+++ flask/app/home/views.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask_login import login_required
 
 from . import home
 from ..models import Usuario
@@ -7,5 +8,11 @@ from ..models import Usuario
 @home.route("/", methods=['GET', 'POST'])
 def index():
     # user = Usuario.query.filter_by(email='daniel@email.com').first()
+    return render_template('base.html')
+
+
+@login_required
+@home.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
     return render_template('base.html')
 
